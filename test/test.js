b51527a8090d9d7866d5c729fa2feb5428663c07
Scott Davis <sc.ttdav.s@gmail.com>
2017-09-24 19:33:36 -0400

    add `get` tests

--- test/test.js
+++ test/test.js
@@ -18,4 +18,20 @@ describe("Radix Trie", () => {
       assert.equal(trie.get("foos"), 9);
     });
   });
+
+  describe("Get", () => {
+    it("gets a value when it exists", () => {
+      const trie = new Trie().add("bar", 15).add("barstool", false);
+
+      assert.equal(trie.get("bar"), 15);
+      assert.equal(trie.get("barstool"), false);
+    });
+
+    it("returns null when a value does not exist", () => {
+      const trie = new Trie().add("bar", 15).add("barstool", false);
+
+      assert.equal(trie.get("barkeep"), null);
+      assert.equal(trie.get("barstool"), false);
+    });
+  })
 });
